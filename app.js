f14813d9fbe03ad47d721fe51816fed6711633ce
Babylatta <111289746+Babylatta@users.noreply.github.com>
2023-06-04 13:52:36 +0900

    #2.2 Line Width (07:10)

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
-
 const ctx = canvas.getContext("2d")
 canvas.width = 800;
 canvas.height= 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMove(event) {
@@ -12,6 +12,7 @@ function onMove(event) {
         ctx.stroke();
         return;
     }
+    ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY); // 이게 있어야 한붓그리기 안됨..! 새로 찍은곳으로 움직이란 거니까!
 }
 
@@ -22,7 +23,13 @@ function cancelPainting(){
     isPainting = false;
 }
 
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value;
+}
+
 canvas.addEventListener("mousemove",onMove);
 canvas.addEventListener("mousedown",startPainting);
 canvas.addEventListener("mouseup",cancelPainting);
 canvas.addEventListener("mouseleave",cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
