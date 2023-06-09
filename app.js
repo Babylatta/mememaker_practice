d6ce7db31f0d00ec63bb9e807fed04bd56a6e14d
Babylatta <111289746+Babylatta@users.noreply.github.com>
2023-06-09 22:28:03 +0900

    #3.1 Adding Text

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const textInput = document.getElementById("text");
+const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraserBtn = document.getElementById("eraser-btn");
@@ -14,6 +16,7 @@ const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height= CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 let isPainting = false;
 let isFilling = false;
 
@@ -76,6 +79,30 @@ function onEraserClick() {
 
 }
 
+function onFileChange(event) {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file)
+    const image = new Image();
+    image.src=url;
+    image.onload = function() {
+        ctx.drawImage(image,0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+        fileInput.value=null;
+    }
+}
+
+function onDoubleClick(event) {
+    const text = textInput.value;
+    if (text !== ""){    
+        ctx.save();
+        ctx.lineWidth = 1;
+        ctx.font = "68px serif";
+        ctx.fillText(text, event.offsetX, event.offsetY);
+        ctx.restore();
+    }
+
+}
+
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove",onMove);
 canvas.addEventListener("mousedown",startPainting);
 canvas.addEventListener("mouseup",cancelPainting);
@@ -94,3 +121,4 @@ colorOptions.forEach(color => color.addEventListener("click",onColorClick));
 modeBtn.addEventListener("click",onModeClick);
 destroyBtn.addEventListener("click",onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change",onFileChange);
